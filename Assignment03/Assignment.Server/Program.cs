e8750322f0550dd979efb6dd5d2e34294a956729
zhenlei520 <wangzhenlei520@gmail.com>
2022-04-17 21:41:27 +0800

    chore: Added Masa.Utils.Development.Dapr.AspNetCore

--- Assignment03/Assignment.Server/Program.cs
+++ Assignment03/Assignment.Server/Program.cs
@@ -1,4 +1,7 @@
+using Masa.Utils.Development.Dapr.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDaprStarter();
 var app = builder.Build();
 
 app.Map("/hello", () => Console.WriteLine("Hello World!"));
